90f330952419601f68c5b3b07b36517030a0847d
Jianlong Nie <1317272927@qq.com>
2021-06-21 17:55:46 +0800

    top header done

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import createStore from '../store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
+library.add(fab, faCheckSquare, faCoffee);
 // Main SCSS
 import '../assets/scss/main.scss';
 // import 'tailwindcss/tailwind.css'
